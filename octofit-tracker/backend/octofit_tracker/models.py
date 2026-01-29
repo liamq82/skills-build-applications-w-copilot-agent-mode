ada257204c1605e1cfb7aff8a32a6515a259c99d
Liam T Quinlan <liamq82@gmail.com>
2026-01-29 18:06:54 +0000

    update

---
+++ octofit-tracker/backend/octofit_tracker/models.py
@@ -0,0 +1,29 @@
+from djongo import models
+from django.contrib.auth import get_user_model
+
+class Team(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    def __str__(self):
+        return self.name
+
+class Activity(models.Model):
+    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    type = models.CharField(max_length=50)
+    duration = models.IntegerField()
+    distance = models.FloatField()
+    def __str__(self):
+        return f"{self.user.username} - {self.type}"
+
+class Workout(models.Model):
+    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    duration = models.IntegerField()
+    def __str__(self):
+        return f"{self.user.username} - {self.name}"
+
+class Leaderboard(models.Model):
+    team = models.ForeignKey(Team, on_delete=models.CASCADE)
+    points = models.IntegerField()
+    def __str__(self):
+        return f"{self.team.name} - {self.points}"
---
+++ octofit-tracker/backend/octofit_tracker/serializers.py
@@ -0,0 +1,34 @@
+from rest_framework import serializers
+from django.contrib.auth import get_user_model
+from .models import Team, Activity, Workout, Leaderboard
+
+User = get_user_model()
+
+class TeamSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Team
+        fields = '__all__'
+
+class UserSerializer(serializers.ModelSerializer):
+    team = TeamSerializer(read_only=True)
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'email', 'team']
+
+class ActivitySerializer(serializers.ModelSerializer):
+    user = UserSerializer(read_only=True)
+    class Meta:
+        model = Activity
+        fields = '__all__'
+
+class WorkoutSerializer(serializers.ModelSerializer):
+    user = UserSerializer(read_only=True)
+    class Meta:
+        model = Workout
+        fields = '__all__'
+
+class LeaderboardSerializer(serializers.ModelSerializer):
+    team = TeamSerializer(read_only=True)
+    class Meta:
+        model = Leaderboard
+        fields = '__all__'
